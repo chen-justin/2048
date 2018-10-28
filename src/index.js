bfdabf96da3a74f375c1c9825d2370b2aa7da544
chen-justin <jnzheng8@gmail.com>
2018-10-27 23:38:13 -0400

    Implemented game's logic.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Tile(props) {
+  let tags = "tile";
+  if (props.value < 0) { //For our purposes, negative values will be the "empty" value.
+    return(
+      <div className="tile">
+        {null}
+      </div>
+    );
+  }
+
+  tags = "tile tile-"+ props.value.toString();
+
   return (
-    <div className="tile">
+    <div className={tags}>
       {props.value}
     </div>
   );
@@ -58,16 +69,21 @@ class Game extends React.Component {
     this.state = {
       history: [
         {
-          tiles: Array(16).fill(null)
+          tiles: Array(16).fill(-1)
         }
       ],
       score: 0,
       best_score: 0,
       stepNumber: 0,
     }
-    const arr = []
+    let arr = []
+    let rows = 4;
+    let rowSize = 4;
+
+
+    //Initializes board with "2" in two random places.
     while (arr.length < 2) {
-      const rand = Math.floor(Math.random()*16);
+      const rand = Math.floor(Math.random()*rows*rowSize);
       if(arr.indexOf(rand) > -1) continue;
       arr[arr.length] = rand;
     }
@@ -75,57 +91,228 @@ class Game extends React.Component {
     this.state.history[0].tiles[arr[1]] = 2;
   }
 
+  handleKeyPress = (e) => {
+    e = e || window.event;
+    if (e.keyCode === 38) {
+      // up arrow
+      this.shift(1);
+    }
+    else if (e.keyCode === 40) {
+      // down arrow
+      this.shift(2);
+    }
+    else if (e.keyCode === 37) {
+      // left arrow
+      this.shift(0);
+    }
+    else if (e.keyCode === 39) {
+      // right arrow
+      this.shift(3);
+    }
+  }
+
   shift(bit) {
     // 0 = left, 1 = up, 2 = down, 3 = right
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const tiles = current.tiles.slice();
-
-    const rowSize = 4;
     const rows = 4;
-    console.log("Shifted");
+    const rowSize = 4;
+    let moveMade = false;
+
     if (bit === 0) {
-      for (let i = 1; i <= rows; i++) {
-        let ptr = i*rowSize - 1;
-        while (ptr > (i-1)*rowSize) {
-          let currTile = tiles[ptr];
-          let nextTile = tiles[ptr-1];
-          if (currTile != null) {
-            if (nextTile === null) {
-              tiles[ptr - 1] = currTile;
-              tiles[ptr] = null;
-            } else {
-              if (nextTile === currTile) {
-                tiles[ptr-1] = currTile*2;
-                tiles[ptr] = null;
-              }
+      //Shifting left
+      for (let i = 0; i < rows; i++) {
+        //Perform possible merges.
+        let prev = i*rowSize;
+        let ptr = prev + 1;
+        while (ptr < (i+1)*rowSize) {
+          if(tiles[ptr] > 0 && tiles[ptr] === tiles[prev]) {
+            this.setState({score: this.state.score + tiles[ptr]});
+            tiles[prev] = tiles[prev]*2;
+            tiles[ptr] = -1;
+            prev = ptr;
+            moveMade = true;
+          } else if (tiles[ptr] > 0) {
+            prev = ptr;
+          }
+          ptr++;
+        }
+
+        //Moves everything as left as possible
+        let pivot = i*rowSize;
+        while (pivot < (i+1)*rowSize && tiles[pivot] > 0) {
+          pivot++;
+        }
+        ptr = pivot; //Reset ptr
+        while (ptr < (i+1)*rowSize) {
+          if (tiles[ptr] > 0) {
+            tiles[pivot] = tiles[ptr];
+            tiles[ptr] = -1;
+            pivot++;
+            moveMade = true;
+          }
+          ptr++;
+        }
+      }
+
+    } else if (bit === 1) {
+      //Shifting up
+      for (let i = 0; i < rowSize; i++) {
+
+        //Perform possible merges
+        let prev = i;
+        let ptr = prev + rowSize;
+        while (ptr < tiles.length) {
+          if(tiles[ptr] > 0 && tiles[ptr] === tiles[prev]) {
+            this.setState({score: this.state.score + tiles[ptr]});
+            tiles[prev] = tiles[prev]*2;
+            tiles[ptr] = -1;
+            prev = ptr;
+            moveMade = true;
+          } else {
+            if (tiles[ptr] > 0) {
+              prev = ptr;
             }
+          }
+          ptr += rowSize;
+        }
+
+        let pivot = i;
+        while (pivot < tiles.length && tiles[pivot] > 0) {
+          pivot += rowSize;
+        }
+        ptr = pivot;
+        while (ptr < tiles.length) {
+          if (tiles[ptr] > 0) {
+            tiles[pivot] = tiles[ptr];
+            tiles[ptr] = -1;
+            pivot += rowSize;
+            moveMade = true;
+          }
+          ptr += rowSize;
+        }
+      }
+    } else if (bit === 2) {
+      //Shifting down
+      for (let i = 0; i < rowSize; i++) {
+
+        //Perform possible merges
+        let prev = tiles.length - i - 1;
+        let ptr = prev - rowSize;
+        while (ptr >= 0) {
+          if(tiles[ptr] > 0 && tiles[ptr] === tiles[prev]) {
+            this.setState({score: this.state.score + tiles[ptr]});
+            tiles[prev] = tiles[prev]*2;
+            tiles[ptr] = -1;
+            prev = ptr;
+            moveMade = true;
+          } else {
+            if (tiles[ptr] > 0) {
+              prev = ptr;
+            }
+          }
+          ptr -= rowSize;
+        }
+
+        let pivot = tiles.length - i - 1;
+        while (pivot >= 0 && tiles[pivot] > 0) {
+          pivot -= rowSize;
+        }
+        ptr = pivot;
+        while (ptr >= 0) {
+          if (tiles[ptr] > 0) {
+            tiles[pivot] = tiles[ptr];
+            tiles[ptr] = -1;
+            pivot -= rowSize;
+            moveMade = true;
+          }
+          ptr -= rowSize;
+        }
+      }
+    } else if (bit === 3) {
+      //Shifting right
+      for (let i = 0; i < rows; i++) {
+        //Perform possible merges.
+        let prev = (i+1)*rowSize-1;
+        let ptr = prev - 1;
+        while (ptr > i*rowSize-1) {
+          if(tiles[ptr] > 0 && tiles[ptr] === tiles[prev]) {
+            this.setState({score: this.state.score + tiles[ptr]});
+            tiles[prev] = tiles[prev]*2;
+            tiles[ptr] = -1;
+            prev = ptr;
+            moveMade = true;
+          } else if (tiles[ptr] > 0) {
+            prev = ptr;
+          }
+          ptr--;
+        }
+
+        //Moves everything as right as possible
+        let pivot = (i+1)*rowSize-1;
+        while (pivot >= i*rowSize && tiles[pivot] > 0) {
+          pivot--;
+        }
+        ptr = pivot; //Reset ptr
+        while (ptr >= i*rowSize) {
+          if (tiles[ptr] > 0) {
+            tiles[pivot] = tiles[ptr];
+            tiles[ptr] = -1;
+            pivot--;
+            moveMade = true;
           }
           ptr--;
         }
       }
     }
 
-    //Generate a new number to add
-    //This is pretty bad right now. Infinite loop if board is full.
-    let rand = Math.floor(Math.random()*16);
-    let iter = 0;
-    while (tiles[rand] != null && iter < 16) {
-      rand = Math.floor(Math.random()*16);
-      iter++;
-    }
-    tiles[rand] = 2;
-
-
-
-    this.setState({
-      history: history.concat([
-        {
-          tiles: tiles
+    if (moveMade && this.move_possible(tiles, rows, rowSize)) {
+      //Generate a new tile to add in an empty space.
+      let emptySpaces = [];
+      for (let i = 0; i < tiles.length; i++) {
+        if (tiles[i] < 0) {
+          emptySpaces.push(i);
         }
-      ]),
-      stepNumber: history.length,
-    })
+      }
+      if (emptySpaces) {
+        let rand = Math.floor(Math.random()*emptySpaces.length);
+        tiles[emptySpaces[rand]] = 2;
+      }
+
+      this.setState({
+        history: history.concat([
+          {
+            tiles: tiles
+          }
+        ]),
+        stepNumber: history.length,
+      })
+    }
+  }
+
+  move_possible(tiles, numRows, numPerRow) {
+    let move_possible = false;
+    for (let i = 0; i < numRows; i++) {
+      const row_index = i*numRows;
+      for (let j = 0; j < numPerRow; j++) {
+        const curr_index = row_index + j;
+
+        //Checks if curr_index - numPerRow is still within bounds of our array.
+        //Sets it to -1 if its out of bounds so we can account for it later.
+        //Necessary since an empty space in our board is "null".
+        //Possibly could make our default "empty" value -1.
+        const above_curr = curr_index - numPerRow > 0 ? curr_index - numPerRow : null;
+        const below_curr = curr_index + numPerRow < tiles.length ? curr_index + numPerRow : null;
+        const left_curr = j - 1 > 0 ? curr_index - 1 : null;
+        const right_curr = j + 1 < numPerRow ? curr_index + 1 :null;
+
+        move_possible = move_possible || above_curr !== null || below_curr !== null || left_curr !== null || right_curr !== null;
+
+      }
+    }
+    console.log(move_possible);
+    return move_possible;
   }
 
   render() {
@@ -134,12 +321,15 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
 
+    document.onkeydown = this.handleKeyPress;
+
     return(
-      <div className="game">
+      <div className="game" onKeyPress={this.handleKeyPress}>
         <div className = "game-info">
           <div>2048 Clone</div>
           <div>Score: {score}</div>
           <div>Best: {best_score}</div>
+          <div>Current Moves: {this.state.stepNumber}</div>
         </div>
         <Board
           tiles={current.tiles}
