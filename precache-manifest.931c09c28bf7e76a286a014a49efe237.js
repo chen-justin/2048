bb43fec5cbc594e5a9bab19ea67d6c3853a63d6a
chen-justin <jnzheng8@gmail.com>
2018-10-28 00:03:25 -0400

    Updates

---
+++ precache-manifest.931c09c28bf7e76a286a014a49efe237.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca6212a0648b1ec2c76b",
+    "url": "/2048/static/js/runtime~main.ca6212a0.js"
+  },
+  {
+    "revision": "e03b2fa5a3d5032bb3dc",
+    "url": "/2048/static/js/main.e03b2fa5.chunk.js"
+  },
+  {
+    "revision": "499bf8bcb9159cbc401f",
+    "url": "/2048/static/js/1.499bf8bc.chunk.js"
+  },
+  {
+    "revision": "e03b2fa5a3d5032bb3dc",
+    "url": "/2048/static/css/main.e058b515.chunk.css"
+  },
+  {
+    "revision": "f2db9913709323afe22770aa4dbedafa",
+    "url": "/2048/index.html"
+  }
+];
