41c3b2b0c636b309e6033a9a7b2914fe32bdbcf4
Justin Chen <jnzheng8@gmail.com>
2018-10-30 15:24:53 -0400

    Disabled arrow key and spacebar scrolling.

--- src/index.js
+++ src/index.js
@@ -455,5 +455,10 @@ class Game extends React.Component {
 }
 
 // ========================================
-
+window.addEventListener("keydown", function(e) {
+  // space and arrow keys
+  if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+      e.preventDefault();
+  }
+}, false);
 ReactDOM.render(<Game />, document.getElementById("root"));
